b13cf6bddb38d66d1439abf2644395f015f542bb
Alexandre Normand <alexandre.normand@gmail.com>
2018-06-23 15:29:18 -0700

    Add Partial Authorization Implementation

--- glukloader/ViewController.swift
+++ glukloader/ViewController.swift
@@ -7,14 +7,67 @@
 //
 
 import UIKit
+import OAuth2
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    var loader: OAuth2DataLoader?
+    
+    var oauth2 = OAuth2CodeGrant(settings: [
+        "client_id": GlukitSecrets.clientId,
+        "client_secret": GlukitSecrets.clientSecret,
+        "consumer_key": GlukitSecrets.clientId,
+        "consumer_secret": GlukitSecrets.clientSecret,
+        "authorize_uri": "https://glukit.appspot.com/authorize",
+        "token_uri": "https://glukit.appspot.com/token",
+        "scope": "",
+        "redirect_uris": ["x-glukloader://oauth/callback"],
+        "keychain": true
+        ] as OAuth2JSON)
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        if oauth2.isAuthorizing {
+            oauth2.abortAuthorization()
+            return
+        }
+        
+        oauth2.authConfig.authorizeEmbedded = true
+        oauth2.authConfig.authorizeContext = self
+        oauth2.logger = OAuth2DebugLogger(.trace)
+        oauth2.authorize(callback:{ authParameters, error in
+            if let params = authParameters {
+                print("Authorized! Access token is in `oauth2.accessToken`")
+                print("Authorized! Additional parameters: \(params)")
+            }
+            else {
+                print("Authorization was canceled or went wrong: \(String(describing: error))")   // error will not be nil
+            }
+        })
     }
 
+    func application(_ app: UIApplication,
+                     open url: URL,
+                     options: [UIApplicationOpenURLOptionsKey: Any] = [:]) -> Bool {
+        // you should probably first check if this is the callback being opened
+        // if your oauth2 instance lives somewhere else, adapt accordingly
+        oauth2.handleRedirectURL(url)
+        
+        return true
+    }
+    
+    func didGetUserdata(dict: [String: Any], loader: OAuth2DataLoader?) {
+        print("Got data: \(dict)")
+    }
+    
+    func didCancelOrFail(_ error: Error?) {
+        DispatchQueue.main.async {
+            if let error = error {
+                print("Authorization went wrong: \(error)")
+            }
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
